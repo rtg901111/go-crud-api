0fc68f179d53b68367a6783624e0d3a30cf0b4f3
Sanghun Kim <rtg901111@gmail.com>
2023-03-06 21:19:15 -0800

    Add connection to Postgres DB

--- initializers/database.go
+++ initializers/database.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,7 +12,7 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	dsn := "host=localhost user=postgres password=postgres dbname=go-api-practice port=5432 sslmode=disable"
+	dsn := os.Getenv("DB_URL")
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
